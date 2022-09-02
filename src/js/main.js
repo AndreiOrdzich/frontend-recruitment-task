3a247313201d0f6ebe1179eb425314a9763d5b01
AndreiOrdzich <dronpoet@mail.ru>
2022-09-02 20:18:10 +0300

    feat: add script for popup count click button

--- src/js/main.js
+++ src/js/main.js
@@ -3,16 +3,19 @@ let popupBg = document.querySelector('.popup-bg'); // Фон попап окна
 let popup = document.querySelector('.popup'); // Само окно
 let openPopupButtons = document.querySelectorAll('.button'); // Кнопки для показа окна
 let closePopupButton = document.querySelector('.close-popup'); // Кнопка для скрытия окна
-
+let counter = document.querySelector('.counter');
 var main = document.querySelector('main');
-var lockPaddingValue = window.innerWidth - document.querySelector('.main').offsetWidth + 'px'; // подсчет толщины скролла
+var lockPaddingValue = window.innerWidth - document.querySelector('.main').offsetWidth + 'px'; // подсчет толщины скролл
+
+
 
 /*start Popup*/
 openPopupButtons.forEach((button) => { // Перебираем все кнопки
     button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
+        let counter = document.querySelector('.counter');
+        counter.innerHTML = (counter.innerHTML * 1) + 1;
         e.preventDefault(); // Предотвращаем дефолтное поведение браузера
         body.style.paddingRight = lockPaddingValue;
-        button.classList.add('opasity-link'); // скрываем нажатую ссылку
         popupBg.classList.add('active'); // Добавляем класс 'active' для фона
         popup.classList.add('active'); // И для самого окна
         document.body.style.overflowY = "hidden"; // убираем возможность прокручивать документ
@@ -22,9 +25,6 @@ openPopupButtons.forEach((button) => { // Перебираем все кнопк
 closePopupButton.addEventListener('click', () => { // Вешаем обработчик на крестик
     popupBg.classList.remove('active'); // Убираем активный класс с фона
     popup.classList.remove('active'); // И с окна
-    openPopupButtons.forEach((button) => {
-        button.classList.remove('opasity-link'); // возвращаем видимость нажатой ссылке
-    });
     setTimeout(function() {
         body.style.overflowY = "visible"; // возвращаем возможность прокрутки документа
         body.style.paddingRight = '0px'; // возвращаем нулевой отступ для body
@@ -35,9 +35,6 @@ document.addEventListener('click', (e) => { // Вешаем обработчик
     if (e.target === popupBg) { // Если цель клика - фон, то:
         popupBg.classList.remove('active'); // Убираем активный класс с фона
         popup.classList.remove('active'); // И с окна
-        openPopupButtons.forEach((button) => {
-            button.classList.remove('opasity-link'); // возвращаем видимость нажатой ссылке
-        })
         setTimeout(function() {
             body.style.overflowY = "visible"; // возвращаем возможность прокрутки документа
             body.style.paddingRight = '0px'; // возвращаем нулевой отступ для body
