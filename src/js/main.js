041bb80097c12ce4a4aa1fbe278ce60b7c34ac75
AndreiOrdzich <dronpoet@mail.ru>
2022-09-03 19:20:52 +0300

    feat: add button-reset animation and script

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,12 @@ let popup = document.querySelector('.popup'); // Само окно
 let openPopupButtons = document.querySelectorAll('.button'); // Кнопки для показа окна
 let closePopupButton = document.querySelector('.close-popup'); // Кнопка для скрытия окна
 let counter = document.querySelector('.counter');
+const buttonReset = document.querySelector('.button-reset')
 var main = document.querySelector('main');
 var lockPaddingValue = window.innerWidth - document.querySelector('.main').offsetWidth + 'px'; // подсчет толщины скролл
 let counter1 = 0;
 let counter2 = 0;
+
 
 /*start Popup*/
 openPopupButtons.forEach((button) => { // Перебираем все кнопки
@@ -15,11 +17,34 @@ openPopupButtons.forEach((button) => { // Перебираем все кнопк
         if(e.target.classList.contains("button1")){
                 counter1++;
                  counter.innerHTML = counter1;
+                 if (counter1 === 5) {
+                    buttonReset.setAttribute('style', 'opacity: 1; transform: translate(20px, 185%)');
+                    buttonReset.addEventListener('click', (e) => {
+                        e.preventDefault();
+                        counter1 = 0;
+                        counter2 = 0;
+                        buttonReset.setAttribute('style', 'opacity: 0; transform: translate(-100%, 0%)');
+                        localStorage.clear();
+                    })
+        
+                }
         }
         if(e.target.classList.contains("button2")){
                 counter2++;
                 counter.innerHTML = counter2;
+                if (counter2 === 5) {
+                    buttonReset.setAttribute('style', 'opacity: 1; transform: translate(20px, 185%)');
+                    buttonReset.addEventListener('click', (e) => {
+                        e.preventDefault();
+                        counter1 = 0;
+                        counter2 = 0;
+                        buttonReset.setAttribute('style', 'opacity: 0; transform: translate(-100%, 0%)');
+                        localStorage.clear();
+                    })
+        
+                }
         }
+
         e.preventDefault(); // Предотвращаем дефолтное поведение браузера
         body.style.paddingRight = lockPaddingValue;
         popupBg.classList.add('active'); // Добавляем класс 'active' для фона
@@ -42,12 +67,15 @@ document.addEventListener('click', (e) => { // Вешаем обработчик
     if (e.target === popupBg) { // Если цель клика - фон, то:
         popupBg.classList.remove('active'); // Убираем активный класс с фона
         popup.classList.remove('active'); // И с окна
+      
         setTimeout(function() {
             body.style.overflowY = "visible"; // возвращаем возможность прокрутки документа
             body.style.paddingRight = '0px'; // возвращаем нулевой отступ для body
         }, 500);
     }
 });
+
+
 /*end Popup*/
 
 //local storage
@@ -73,3 +101,5 @@ function getLocalStorage() {
 window.addEventListener('load', getLocalStorage);
 window.addEventListener('beforeunload', setLocalStorage);
 // localStorage.clear();
+
+localStorage.clear();
